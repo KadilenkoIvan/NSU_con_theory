cc6eae6d285a9a894b2bc40ca5249ef79a5824d3
KadilenkoIvan <kadilenkoivan@gmail.com>
2024-02-12 17:19:48 +0700

    git push X

---
+++ task 1/float/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ivank/parrallel/1_task
+# Build directory: /home/ivank/parrallel/1_task/float
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
