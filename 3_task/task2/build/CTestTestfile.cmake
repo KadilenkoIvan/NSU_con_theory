920d4621f1d219dc5665029625cde115f2f9eb86
KadilenkoIvan <kadilenkoivan@gmail.com>
2024-03-16 22:37:18 +0700

    task 3.2

---
+++ 3_task/task2/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Users/IvanK/Desktop/parrallel/3_task/task2
+# Build directory: D:/Users/IvanK/Desktop/parrallel/3_task/task2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
